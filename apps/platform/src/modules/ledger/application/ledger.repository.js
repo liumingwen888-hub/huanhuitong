7d2d32183e4c00a566d5c7ab46571d3c88e8065d
aemfff@gmail.com <laizhichao888@gmail.com>
2026-08-19 10:54:27 -0400

    chore: gitignore platform src dist 编译产物（S3-5 提交泄漏清理）

--- apps/platform/src/modules/ledger/application/ledger.repository.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ledger.repository.js.map
--- apps/platform/src/modules/ledger/domain/ledger.errors.d.ts
+++
@@ -1,7 +0,0 @@
-import type { LedgerContractErrorCode } from '@xht/contracts';
-export declare class LedgerError extends Error {
-    readonly code: LedgerContractErrorCode;
-    readonly retryable: false;
-    constructor(code: LedgerContractErrorCode);
-}
-export declare function isAuthenticLedgerError(value: unknown): value is LedgerError;
